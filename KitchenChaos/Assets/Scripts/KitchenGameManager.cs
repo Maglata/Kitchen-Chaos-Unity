16c0f1feda24e0ed71244fe6621553114292ca3d
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2023-07-28 11:56:05 +0300

    Pause + Options and Audio Levels implemented

--- KitchenChaos/Assets/Scripts/KitchenGameManager.cs
+++ KitchenChaos/Assets/Scripts/KitchenGameManager.cs
@@ -8,10 +8,13 @@ public class KitchenGameManager : MonoBehaviour
     [SerializeField] private float waitingToStartTimer = 1f;
     [SerializeField] private float countdownToStartTimer = 3f;
     [SerializeField] private float gamePlayingTimerMax = 10f;
+    [SerializeField] private bool isGamePaused = false;
 
     public static KitchenGameManager Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnPaused;
 
     private enum State
     {
@@ -22,12 +25,22 @@ public class KitchenGameManager : MonoBehaviour
     }
 
     private State state;
-    private float gamePlayingTimer;
+    private float gamePlayingTimer;  
 
     private void Awake()
     {
         state = State.WaitingToStart;
         Instance = this;
+    }
+
+    private void Start()
+    {
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
+    }
+
+    private void GameInput_OnPauseAction(object sender, EventArgs e)
+    {
+        TogglePauseGame();
     }
 
     private void Update()
@@ -74,19 +87,32 @@ public class KitchenGameManager : MonoBehaviour
     {
         return state == State.CountdownToStart;
     }
-
     public float GetCountdownToStartTimer()
     {
         return countdownToStartTimer;
     }
-
     public bool isGameOver()
     {
         return state == State.GameOver;
     }
-
     public float GetGamePlayingTimerNormalized()
     {
         return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+    }
+
+    public void TogglePauseGame()
+    {
+        isGamePaused = !isGamePaused;
+        if(isGamePaused)
+        {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnGameUnPaused?.Invoke(this, EventArgs.Empty);
+        }
+        
     }
 }
--- KitchenChaos/Assets/Scripts/SoundManager.cs
+++ KitchenChaos/Assets/Scripts/SoundManager.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
     public static SoundManager Instance { get; private set; }
 
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
+
     private void Awake()
     {
         Instance = this;
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
 
     private void Start()
@@ -62,13 +66,29 @@ public class SoundManager : MonoBehaviour
     {
         AudioSource.PlayClipAtPoint(audioClip, position, volume);
     }
-    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volumeMultiplier = 1f)
     {
-        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volumeMultiplier * volume);
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume = 1f)
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        if(volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume() { 
+        return volume;
     }
 }
